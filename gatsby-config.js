98ed34aa27b893ce8e2e54b51e50a255115db9c5
Mauro Bono <bono.mauro@gmail.com>
2019-12-09 13:08:01 +0100

    NETLIFY _HEADERS
    added gatsby plugin for managing netlify headers and redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,7 @@ module.exports = {
         icon: `src/static/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
